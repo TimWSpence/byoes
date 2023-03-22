d51c74d55cc8edee9fb39b93d9ba1f3d72020145
Tim Spence <timothywspence@gmail.com>
2023-03-22 17:14:14 +0000

    Implement error handling

--- IO.scala
+++ IO.scala
@@ -18,26 +18,59 @@ enum IO[+A]:
     HandleErrorWith(this, f)
 
   def unsafeRunSync(): A =
-    var conts: List[Any => Any] = Nil
 
+    val flatMapT: Byte = 0
+    val handleErrorT: Byte = 1
+
+    var conts: List[Any => Any] = Nil
+    var tags: List[Byte] = Nil
+
+    @tailrec
     def go(io: IO[Any]): Any = io match
       case Pure(a) =>
+        while (!tags.isEmpty && tags.head != flatMapT) {
+          tags = tags.tail
+          conts = conts.tail
+        }
         conts match
           case Nil => a
           case f :: rest =>
             conts = rest
+            tags = tags.tail
             val next = f(a).asInstanceOf[IO[Any]]
             go(next)
       case Delay(thunk) =>
+        while (!tags.isEmpty && tags.head != flatMapT) {
+          tags = tags.tail
+          conts = conts.tail
+        }
         conts match
           case Nil => thunk()
           case f :: rest =>
             conts = rest
+            tags = tags.tail
             val next = f(thunk()).asInstanceOf[IO[Any]]
             go(next)
       case FlatMap(io, f) =>
         conts = f.asInstanceOf[Any => Any] :: conts
+        tags = flatMapT :: tags
         go(io)
+      case HandleErrorWith(io, f) =>
+        conts = f.asInstanceOf[Any => Any] :: conts
+        tags = handleErrorT :: tags
+        go(io)
+      case RaiseError(e) =>
+        while (!tags.isEmpty && tags.head != handleErrorT) {
+          tags = tags.tail
+          conts = conts.tail
+        }
+        conts match
+          case Nil => throw e
+          case f :: rest =>
+            conts = rest
+            tags = tags.tail
+            val next = f(e).asInstanceOf[IO[Any]]
+            go(next)
 
     go(this).asInstanceOf[A]
 
